d8619d5385bcbc829c96754f394278b178f6efb7
davessh <david.schlemmer@uabc.edu.mx>
2025-09-18 19:00:29 -0700

    Se realizo el cambio de arraylist a pila en la clase FoundationDeck

--- src/main/java/solitaire/FoundationDeck.java
+++ src/main/java/solitaire/FoundationDeck.java
@@ -2,6 +2,7 @@ package solitaire;
 
 import DeckOfCards.CartaInglesa;
 import DeckOfCards.Palo;
+import DeckOfCards.Pila;
 
 import java.util.ArrayList;
 
@@ -14,17 +15,20 @@ import java.util.ArrayList;
  */
 public class FoundationDeck {
     Palo palo;
-    ArrayList<CartaInglesa> cartas = new ArrayList<>();
+    //ArrayList<CartaInglesa> cartas = new ArrayList<>();
+    Pila<CartaInglesa> cartas;
 
     public FoundationDeck(Palo palo) {
         this.palo = palo;
+        this.cartas = new Pila<>(13);
     }
 
     public FoundationDeck(CartaInglesa carta) {
         palo = carta.getPalo();
+        this.cartas = new Pila<>(13);
         // solo agrega la carta si es un A
         if (carta.getValorBajo() == 1) {
-            cartas.add(carta);
+            cartas.push(carta);
         }
     }
 
@@ -39,18 +43,18 @@ public class FoundationDeck {
     public boolean agregarCarta(CartaInglesa carta) {
         boolean agregado = false;
         if (carta != null && carta.tieneElMismoPalo(palo)) {
-            if (cartas.isEmpty()) {
+            if (cartas.pilaVacia()) {
                 if (carta.getValorBajo() == 1) {
                     // si no hay cartas entonces la carta debe ser un A
-                    cartas.add(carta);
+                    cartas.push(carta);
                     agregado = true;
                 }
             } else {
                 // si hay cartas entonces debe haber secuencia
-                CartaInglesa ultimaCarta = cartas.getLast();
-                if (ultimaCarta.getValorBajo() + 1 == carta.getValorBajo()) {
+                CartaInglesa ultimaCarta = getUltimaCarta();
+                if (ultimaCarta != null && ultimaCarta.getValorBajo() + 1 == carta.getValorBajo()) {
                     // agregar la carta si el la siguiente a la última
-                    cartas.add(carta);
+                    cartas.push(carta);
                     agregado = true;
                 }
             }
@@ -64,24 +68,30 @@ public class FoundationDeck {
      * @return la carta que removió, null si estaba vacio
      */
     CartaInglesa removerUltimaCarta() {
-        CartaInglesa ultimaCarta = null;
-        if (!cartas.isEmpty()) {
-            ultimaCarta = cartas.getLast();
-            cartas.remove(ultimaCarta);
-        }
-        return ultimaCarta;
+       if(cartas.pilaVacia()) {
+           return null;
+       }
+       return cartas.pop();
     }
 
     @Override
     public String toString() {
         StringBuilder builder = new StringBuilder();
-        if (cartas.isEmpty()) {
-            builder.append("---");
-        } else {
-            for (CartaInglesa carta : cartas) {
-                builder.append(carta.toString());
-            }
-        }
+       if(cartas.pilaVacia()) {
+           builder.append("---");
+       } else {
+           Pila<CartaInglesa> auxiliar = new Pila<>(13);
+
+           while(!cartas.pilaVacia()) {
+               auxiliar.push(cartas.pop());
+           }
+
+           while(!auxiliar.pilaVacia()) {
+               CartaInglesa carta = auxiliar.pop();
+               builder.append(carta.toString());
+               cartas.push(carta);
+           }
+       }
         return builder.toString();
     }
 
@@ -90,7 +100,7 @@ public class FoundationDeck {
      * @return true hay al menos una carta, false no hay cartas
      */
     public boolean estaVacio() {
-        return cartas.isEmpty();
+        return cartas.pilaVacia();
     }
 
     /**
@@ -98,10 +108,11 @@ public class FoundationDeck {
      * @return última carta, null si no hay cartas
      */
     public CartaInglesa getUltimaCarta() {
-        CartaInglesa ultimaCarta = null;
-        if (!cartas.isEmpty()) {
-            ultimaCarta = cartas.getLast();
+        if (cartas.pilaVacia()) {
+            return null;
         }
-        return ultimaCarta;
+        CartaInglesa carta = cartas.pop();
+        cartas.push(carta);
+        return carta;
     }
 }
